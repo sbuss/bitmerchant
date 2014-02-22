aa10e37ca371af8e111b9d3711909e151d82edfa
Steven Buss <steven.buss@gmail.com>
2014-02-22 13:11:43 -0800

    Double check version bytes, and docs

--- bitmerchant/network.py
+++ bitmerchant/network.py
@@ -1,25 +1,42 @@
 class BitcoinMainNet(object):
+    """Bitcoin MainNet version bytes.
+
+    From https://github.com/bitcoin/bitcoin/blob/v0.9.0rc1/src/chainparams.cpp
+    """
     NAME = "Bitcoin Main Net"
-    SCRIPT_ADDRESS = 0x04
-    PUBKEY_ADDRESS = 0x00  # = int(0) --> '\0'
-    SECRET_KEY = 0x80  # = int(128) --> "5"
+    SCRIPT_ADDRESS = 0x05  # int(0x05) = 5
+    PUBKEY_ADDRESS = 0x00  # int(0x00) = 0
+    SECRET_KEY = 0x80      # int(0x80) = 128
     EXT_PUBLIC_KEY = 0x0488B21E
     EXT_SECRET_KEY = 0x0488ADE4
 
 
 class BitcoinTestNet(object):
+    """Bitcoin TestNet version bytes.
+
+    From https://github.com/bitcoin/bitcoin/blob/v0.9.0rc1/src/chainparams.cpp
+    """
     NAME = "Bitcoin Test Net"
-    # SCRIPT_ADDRESS = 0x04  TODO Verify this constant
-    PUBKEY_ADDRESS = 0x6f  # = int(111) --> 'o'
-    SECRET_KEY = 0xEF  # = int(239) --> "9"
+    SCRIPT_ADDRESS = 0xc4  # int(0xc4) = 196
+    PUBKEY_ADDRESS = 0x6f  # int(0x6f) = 111
+    SECRET_KEY = 0xEF      # int(0xef) = 239
     EXT_PUBLIC_KEY = 0x043587CF
     EXT_SECRET_KEY = 0x04358394
 
 
 class DogecoinMainNet(object):
-    SCRIPT_ADDRESS = 0x16  # int(0x16) --> 22
-    PUBKEY_ADDRESS = 0x1e  # int(0x1e) --> 30 --> "D"
-    SECRET_KEY = SCRIPT_ADDRESS + 128  # 30 + 128 = 158
+    """Dogecoin MainNet version bytes
+
+    Primary version bytes from:
+    https://github.com/dogecoin/dogecoin/blob/1.5.2/src/base58.h
+
+    Unofficial extended version bytes from
+    https://bitcointalk.org/index.php?topic=409731
+    """
+    NAME = "Dogecoin Main Net"
+    SCRIPT_ADDRESS = 0x16  # int(0x16) = 22
+    PUBKEY_ADDRESS = 0x1e  # int(0x1e) = 30
+    SECRET_KEY = PUBKEY_ADDRESS + 128  # int(0x9e) = 158
 
     # Unofficial extended version bytes taken from
     # https://bitcointalk.org/index.php?topic=409731
@@ -28,9 +45,18 @@ class DogecoinMainNet(object):
 
 
 class DogecoinTestNet(object):
-    SCRIPT_ADDRESS = 0xc4  # int(0xc4) --> 196
-    PUBKEY_ADDRESS = 0x71  # int(0x71) --> 113
-    SECRET_KEY = SCRIPT_ADDRESS + 128  # 30 + 128 = 158
+    """Dogecoin TestNet version bytes
+
+    Primary version bytes from:
+    https://github.com/dogecoin/dogecoin/blob/1.5.2/src/base58.h
+
+    Unofficial extended version bytes from
+    https://bitcointalk.org/index.php?topic=409731
+    """
+    NAME = "Dogecoin Test Net"
+    SCRIPT_ADDRESS = 0xc4  # int(0xc4) = 196
+    PUBKEY_ADDRESS = 0x71  # int(0x71) = 113
+    SECRET_KEY = PUBKEY_ADDRESS + 128  # int(0xf1) = 241
 
     # Unofficial extended version bytes taken from
     # https://bitcointalk.org/index.php?topic=409731
