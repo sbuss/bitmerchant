9495cbf8b8f5bf03f5615e49a27e552e927c05b9
Steven Buss <steven.buss@gmail.com>
2015-07-31 23:33:40 -0700

    Use nosetest test generation to split up the test vector tests

--- tests/test_key_vector.py
+++ tests/test_key_vector.py
@@ -1,20 +1,22 @@
 import json
 
-from unittest import TestCase
-
 from bitmerchant.network import BitcoinMainNet
 from bitmerchant.wallet.keys import PrivateKey
 from bitmerchant.wallet.keys import PublicKey
 
 
-class TestKeys(TestCase):
-    def test_keys(self):
-        with open("tests/keys_test_vector.json", 'r') as f:
-            vectors = json.loads(f.read())
-        for vector in vectors:
-            private_key = PrivateKey.from_wif(
-                vector['private_key'], network=BitcoinMainNet)
-            public_key = PublicKey.from_hex_key(
-                vector['pubkey'], network=BitcoinMainNet)
-            self.assertEqual(private_key.get_public_key(), public_key)
-            self.assertEqual(public_key.to_address(), vector['address'])
+def _test_key(vector):
+    private_key = PrivateKey.from_wif(
+        vector['private_key'], network=BitcoinMainNet)
+    public_key = PublicKey.from_hex_key(
+        vector['pubkey'], network=BitcoinMainNet)
+    assert private_key.get_public_key() == public_key
+    assert public_key.to_address() == vector['address']
+
+
+def test_keys():
+    with open("tests/keys_test_vector.json", 'r') as f:
+        vectors = json.loads(f.read())
+    for vector in vectors:
+        # use yield for nose test generation
+        yield _test_key, vector
